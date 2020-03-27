47933d2a173813012237a5dd2c30e32e1425b116
Alex <githubalexrom@gmail.com>
2020-03-27 11:42:16 -0400

    Initial commit

---
+++ src/main/java/alexlab/MessageStoringFullStackApp/MessageStoringFullStackAppApplication.java
@@ -0,0 +1,13 @@
+package alexlab.MessageStoringFullStackApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStoringFullStackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageStoringFullStackAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexlab/MessageStoringFullStackApp/MessageStoringFullStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package alexlab.MessageStoringFullStackApp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MessageStoringFullStackAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
