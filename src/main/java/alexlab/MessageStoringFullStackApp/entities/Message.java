48b6f47bca3e09fe440b58f7acf8b30e75508177
Alex <githubalexrom@gmail.com>
2020-03-29 10:16:39 -0400

    Create Message.java

---
+++ src/main/java/alexlab/MessageStoringFullStackApp/entities/Message.java
@@ -0,0 +1,46 @@
+package alexlab.MessageStoringFullStackApp.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="messages")
+public class Message {
+	
+	
+	
+	//***********Attributes***************//
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column(nullable=false)
+	private String content;
+
+	
+	//*************Getters&Setteers********//
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
